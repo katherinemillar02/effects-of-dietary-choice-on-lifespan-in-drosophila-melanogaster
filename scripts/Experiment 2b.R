fa8406169596975d10668f4648fc7247dff7877c
katherinemillar02 <ajh20fhu@uea.ac.uk>
2022-10-01 16:28:57 +0100

    Adapted script 2b to have the correct code

--- scripts/Experiment 2b.R
+++ scripts/Experiment 2b.R
@@ -3,13 +3,13 @@
 
 #-----------------------  Mated Females (exp 4, day 1)
 
-mated_femalese4d1 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE2bD1.xlsx", na = "NA"))
+mated_females_e2bd1 <- (read_excel(path = "data/MatedFemalesE2bD1.xlsx", na = "NA"))
 
 
-long_mated_femalese4d1 <- mated_femalese4d1 %>% 
+long_mated_females_e2bd1 <- mated_females_e2bd1 %>% 
   pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
 
-long_mated_femalese4d1_summary <- long_mated_femalese4d1 %>% 
+long_mated_females_e2bd1_summary <- long_mated_females_e2bd1 %>% 
   group_by(diet) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
@@ -18,7 +18,7 @@ long_mated_femalese4d1_summary <- long_mated_femalese4d1 %>%
 
 #---------------- Visualising the data for mated females (exp 4, day 1)
 
-mated_femalese4d1_plot <- long_mated_femalese4d1_summary%>% 
+mated_females_e2bd1_plot <- long_mated_females_e2bd1_summary%>% 
   ggplot(aes(x = diet, y = mean))+
   geom_bar(stat = "identity",
            fill = "red",
@@ -27,7 +27,7 @@ mated_femalese4d1_plot <- long_mated_femalese4d1_summary%>%
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
                 colour = "blue",
                 width = 0.2)+
-  geom_jitter(data = long_mated_femalese4d1,
+  geom_jitter(data = long_mated_females_e2bd1,
               aes(x = diet,
                   y = fly_numbers),
               fill = "skyblue",
@@ -44,13 +44,13 @@ mated_femalese4d1_plot <- long_mated_femalese4d1_summary%>%
 
 #-----------------------  Mated Females (exp 4, day 2)
 
-mated_femalese4d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE2bD2.xlsx", na = "NA"))
+mated_females_e2bd2 <- (read_excel(path = "data/MatedFemalesE2bD2.xlsx", na = "NA"))
 
 
-long_mated_femalese4d2 <- mated_femalese4d2 %>% 
+long_mated_females_e2bd2 <- mated_females_e2bd2 %>% 
   pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
 
-long_mated_femalese4d2_summary <- long_mated_femalese4d2 %>% 
+long_mated_females_e2bd2_summary <- long_mated_females_e2bd2 %>% 
   group_by(diet) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
@@ -59,7 +59,7 @@ long_mated_femalese4d2_summary <- long_mated_femalese4d2 %>%
 
 #----------------- Visualising the data for mated females (exp 4, day 2)
 
-mated_femalese4d2_plot <- long_mated_femalese4d2_summary%>% 
+mated_females_e2bd2_plot <- long_mated_females_e2bd2_summary%>% 
   ggplot(aes(x = diet, y = mean))+
   geom_bar(stat = "identity",
            fill = "red",
@@ -68,7 +68,7 @@ mated_femalese4d2_plot <- long_mated_femalese4d2_summary%>%
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
                 colour = "blue",
                 width = 0.2)+
-  geom_jitter(data = long_mated_femalese4d2,
+  geom_jitter(data = long_mated_females_e2bd2,
               aes(x = diet,
                   y = fly_numbers),
               fill = "skyblue",
@@ -85,13 +85,13 @@ mated_femalese4d2_plot <- long_mated_femalese4d2_summary%>%
 
 #----------------------- Virgin Females (exp 4, day 1)
 
-virgin_femalese4d1 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/VirginFemalesE2bD1.xlsx", na = "NA"))
+virgin_females_e2bd1 <- (read_excel(path = "data/VirginFemalesE2bD1.xlsx", na = "NA"))
 
 
-long_virgin_femalese4d1 <- virgin_femalese4d1 %>% 
+long_virgin_females_e2bd1 <- virgin_females_e2bd1 %>% 
   pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
 
-long_virgin_femalese4d1_summary <- long_virgin_femalese4d1 %>% 
+long_virgin_females_e2bd1_summary <- long_virgin_females_e2bd1 %>% 
   group_by(diet) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
@@ -100,7 +100,7 @@ long_virgin_femalese4d1_summary <- long_virgin_femalese4d1 %>%
 
 #--------------- Visualising the data for virgin females (exp 4, day 1)
 
-virgin_femalese4d1_plot <- long_virgin_femalese4d1_summary%>% 
+virgin_females_e2bd1_plot <- long_virgin_females_e2bd1_summary%>% 
   ggplot(aes(x = diet, y = mean))+
   geom_bar(stat = "identity",
            fill = "red",
@@ -109,7 +109,7 @@ virgin_femalese4d1_plot <- long_virgin_femalese4d1_summary%>%
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
                 colour = "blue",
                 width = 0.2)+
-  geom_jitter(data = long_virgin_femalese4d1,
+  geom_jitter(data = long_virgin_females_e2bd1,
               aes(x = diet,
                   y = fly_numbers),
               fill = "skyblue",
@@ -124,18 +124,18 @@ virgin_femalese4d1_plot <- long_virgin_femalese4d1_summary%>%
 
 #---------------- Data analysis for virgin females (exp 4, day 1)
 
-virgin_femalese4d1ls1 <- lm(fly_numbers ~ diet, data = long_virgin_femalese4d1)
-performance::check_model(virgin_femalese4d1ls1)
+#virgin_females_e4_d1ls1 <- lm(fly_numbers ~ diet, data = long_virgin_females_e4d1)
+#performance::check_model(virgin_females_e4d1_ls1)
 
 #----------------------- Virgin Females (exp 4, day 2)
 
-virgin_femalese4d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/VirginFemalesE2bD2.xlsx", na = "NA"))
+virgin_females_e2bd2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/VirginFemalesE2bD2.xlsx", na = "NA"))
 
 
-long_virgin_femalese4d2 <- virgin_femalese4d2 %>% 
+long_virgin_females_e2bd2 <- virgin_females_e2bd2 %>% 
   pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
 
-virgin_femalese4d2_summary <- long_virgin_femalese4d2 %>% 
+virgin_females_e2bd2_summary <- long_virgin_females_e2bd2 %>% 
   group_by(diet) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
@@ -144,7 +144,7 @@ virgin_femalese4d2_summary <- long_virgin_femalese4d2 %>%
 
 #------------- Visualising the data for virgin females (exp 4, day 2)
 
-virgin_femalese4d2_plot <- long_virgin_femalese4d2_summary%>% 
+virgin_femalese2bd2_plot <- long_virgin_females_e2bd2_summary%>% 
   ggplot(aes(x = diet, y = mean))+
   geom_bar(stat = "identity",
            fill = "red",
@@ -153,7 +153,7 @@ virgin_femalese4d2_plot <- long_virgin_femalese4d2_summary%>%
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
                 colour = "blue",
                 width = 0.2)+
-  geom_jitter(data = long_virgin_femalese4d2,
+  geom_jitter(data = long_virgin_females_e2bd2,
               aes(x = diet,
                   y = fly_numbers),
               fill = "skyblue",
@@ -169,8 +169,8 @@ virgin_femalese4d2_plot <- long_virgin_femalese4d2_summary%>%
 #---------------- Data analysis for virgin females (exp 4, day 2)
 
 #virgin_femalese4d2_summary %>%
-  kbl(caption=" ") %>% 
-  kable_styling(bootstrap_options = "striped", full_width = T, position = "left")
+#kbl(caption=" ") %>% 
+# kable_styling(bootstrap_options = "striped", full_width = T, position = "left")
 
   #virgin_femalese4d2ls1 <- lm(fly_numbers ~ diet, data = long_virgin_femalese4d2)
 
@@ -199,5 +199,57 @@ virgin_femalese4d2_plot <- long_virgin_femalese4d2_summary%>%
 
 
 
-
+  
+  #----------------------OVERALL DATA ANALYSIS FOR EXPERIMENT 2b ----------------#
+  
+  # Mutating a sex variable 
+  exp2bvirgin <- long_virgin_females_e %>% mutate(sex = "virgin")
+  
+  
+  exp2bmated <- long_female_feedingd1 %>% mutate(sex = "mated")
+  
+  
+  #using r bind to make two data sets one data set 
+  
+  
+  exp2b <- rbind(exp2bvirgin, exp2bmated)
+  
+  # Making a linear model
+  exp2bls1 <- lm(fly_numbers ~ diet + sex, data = exp2b)
+  
+  performance::check_model(exp1ls1)
+  
+  broom::tidy(exp1ls1,  
+              exponentiate=T, 
+              conf.int=T)
+  
+  # Same linear model but with sqrt 
+  exp1ls1a <- lm(sqrt(fly_numbers) ~ diet + sex , data = exp1)
+  
+  performance::check_model(exp1ls1a)
+  
+  broom::tidy(exp1ls1a,  
+              exponentiate=T, 
+              conf.int=T)
+  
+  
+  # Making a second linear model (with an interaction effect)
+  
+  exp1ls2 <- lm(fly_numbers ~ diet * sex, data = exp1)
+  
+  performance::check_model(exp1ls2)
+  
+  broom::tidy(exp1ls2,  
+              exponentiate=T, 
+              conf.int=T)
+  
+  # Same linear model but with sqrt 
+  
+  exp1ls2a <- lm(sqrt(fly_numbers) ~ diet * sex , data = exp1)
+  
+  performance::check_model(exp1ls2a)
+  
+  broom::tidy(exp1ls2a,  
+              exponentiate=T, 
+              conf.int=T)
 
