82c7c25a2dbbe020d24bb232bb7545d57b62bb23
katherinemillar02 <ajh20fhu@uea.ac.uk>
2022-10-04 21:28:53 +0100

    Made new plots with collated days for experiment 3.

--- scripts/Experiment 3.R
+++ scripts/Experiment 3.R
@@ -2,25 +2,18 @@
 
 #-----------------  Feeding behaviour analysis 
 
-#--------------------- Mated females (exp 3, day 1)
-
-mated_femalese3d1 <- (read_excel(path = "data/MatedFemalesE3D1.xlsx", na = "NA"))
-
-
-long_mated_femalese3d1 <- mated_femalese3d1 %>% 
+#----------------- FEMALE FEEDING BEHAVIOUR (Days 1 & 2)
+long_exp3femalesall <- exp3femalesall %>% 
   pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
 
-long_mated_femalese3d1_summary <- long_mated_femalese3d1 %>% 
+exp3femalesall_summary <- exp3femalesall %>% 
   group_by(diet) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
             n = n(),
             se = sd/sqrt(n))
 
-
-#--------------------- Visualising the data for mated females (exp 3, day 1)
-
-mated_femalese3d1_plot <- long_mated_femalese3d1_summary%>% 
+exp3femalesall_plot <- exp3femalesall_summary%>% 
   ggplot(aes(x = diet, y = mean))+
   geom_bar(stat = "identity",
            fill = "skyblue",
@@ -29,48 +22,7 @@ mated_femalese3d1_plot <- long_mated_femalese3d1_summary%>%
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
                 colour = "orange",
                 width = 0.2)+
-  geom_jitter(data = long_mated_femalese3d1,
-              aes(x = diet,
-                  y = fly_numbers),
-              fill = "skyblue",
-              colour = "black",
-              width = 0.2,
-              shape = 21)+
-  ylim(0,6)+ 
-  labs(x = "Diet \n(Protein; Carbohydrate)",
-       y = "Mean (+/- S.E.) number of flies")+
-  theme_minimal()
-
-#--------- Data analysis for mated females (exp 3, day 1)
-
-
-
-#--------------------- Mated females (exp 3, day 2)
-
-mated_femalese3d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE3D2.xlsx", na = "NA"))
-
-long_mated_femalese3d2 <- mated_femalese3d2 %>% 
-  pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
-
-long_mated_femalese3d2_summary <- long_mated_femalese3d2 %>% 
-  group_by(diet) %>% 
-  summarise(mean = mean(fly_numbers),
-            sd = sd(fly_numbers),
-            n = n(),
-            se = sd/sqrt(n))
-
-#--------------  Visualising the data for mated females (exp 3, day 2)
-
-mated_femalese3d2_plot <- long_mated_femalese3d2_summary%>% 
-  ggplot(aes(x = diet, y = mean))+
-  geom_bar(stat = "identity",
-           fill = "skyblue",
-           colour = "orange",
-           alpha = 0.6)+
-  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
-                colour = "orange",
-                width = 0.2)+
-  geom_jitter(data = long_mated_femalese3d2,
+  geom_jitter(data = exp3femalesall,
               aes(x = diet,
                   y = fly_numbers),
               fill = "skyblue",
@@ -83,166 +35,40 @@ mated_femalese3d2_plot <- long_mated_femalese3d2_summary%>%
   theme_minimal()
 
 
-#-------- Data analysis for mated females (exp 3, day 2)
 
 
 
-#---------------- Males + Females ----- -----------
 
-#----------------------------- Males (m+f) (exp 3, day 1)
-
-males_mf_e3_d1 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedMalesE3D1(M+F).xlsx", na = "NA"))
-
-long_males_mf_e3_d1 <- males_mf_e3_d1 %>% 
+long_exp3bothall <- exp3bothall %>% 
   pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
 
-males_mf_e3_d1_summary <- long_males_mf_e3_d1 %>% 
+exp3bothall_summary <- exp3bothall %>% 
   group_by(diet) %>% 
   summarise(mean = mean(fly_numbers),
             sd = sd(fly_numbers),
             n = n(),
             se = sd/sqrt(n))
 
-#------------------- Visualising the data for males (exp 3, day 1)
-
-males_mf_e3_d1_plot <- males_mf_e3_d1_summary  %>% 
+exp3both_plot <- exp3bothall_summary%>% 
   ggplot(aes(x = diet, y = mean))+
   geom_bar(stat = "identity",
-           fill = "red",
-           colour = "blue",
+           fill = "skyblue",
+           colour = "orange",
            alpha = 0.6)+
   geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
-                colour = "blue",
+                colour = "orange",
                 width = 0.2)+
-  geom_jitter(data = long_males_mf_e3_d1,
+  geom_jitter(data = exp3bothall,
               aes(x = diet,
                   y = fly_numbers),
               fill = "skyblue",
               colour = "black",
               width = 0.2,
               shape = 21)+
-  ylim(0,2)+ 
+  ylim(0,6)+ 
   labs(x = "Diet \n(Protein; Carbohydrate)",
        y = "Mean (+/- S.E.) number of flies")+
   theme_minimal()
-
-
-#------------------------- Females (m+f) (exp 3, day 1) 
-
-females_mf_e3_d1 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE3D1(M+F).xlsx", na = "NA"))
-
-long_females_mf_e3_d1 <- females_mf_e3_d1 %>% 
-  pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
-
-females_mf_e3_d1_summary <- long_females_mf_e3_d1 %>% 
-  group_by(diet) %>% 
-  summarise(mean = mean(fly_numbers),
-            sd = sd(fly_numbers),
-            n = n(),
-            se = sd/sqrt(n))
-
-#---------------------- Visualising the data for females (exp 3, day 1)
-
-females_mf_e3_d1_plot <- females_mf_e3_d1_summary  %>% 
-  ggplot(aes(x = diet, y = mean))+
-  geom_bar(stat = "identity",
-           fill = "red",
-           colour = "blue",
-           alpha = 0.6)+
-  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
-                colour = "blue",
-                width = 0.2)+
-  geom_jitter(data = long_females_mf_e3_d1,
-              aes(x = diet,
-                  y = fly_numbers),
-              fill = "skyblue",
-              colour = "black",
-              width = 0.2,
-              shape = 21)+
-  ylim(0,2)+ 
-  labs(x = "Diet \n(Protein; Carbohydrate)",
-       y = "Mean (+/- S.E.) number of flies")+
-  theme_minimal()
-
-#-------- Plot showing males and females together (Day 1)
-
-males_mf_e3_d1_plot + females_mf_e3_d1_plot
-
-#-------- Males (m+f) (exp 3,Day 2)
-
-males_mf_e3_d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedMalesE3D2(M+F).xlsx", na = "NA"))
-
-long_males_mf_e3_d2 <- males_mf_e3_d2 %>% 
-  pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
-
-males_mf_e3_d2_summary <- long_males_mf_e3_d2 %>% 
-  group_by(diet) %>% 
-  summarise(mean = mean(fly_numbers),
-            sd = sd(fly_numbers),
-            n = n(),
-            se = sd/sqrt(n))
-
-#--------  Visualising the data for males (m+f) (exp 3, day 2)
-
-males_mf_e3_d2_plot <- males_mf_e3_d2_summary  %>% 
-  ggplot(aes(x = diet, y = mean))+
-  geom_bar(stat = "identity",
-           fill = "red",
-           colour = "blue",
-           alpha = 0.6)+
-  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
-                colour = "blue",
-                width = 0.2)+
-  geom_jitter(data = long_males_mf_e3_d2,
-              aes(x = diet,
-                  y = fly_numbers),
-              fill = "skyblue",
-              colour = "black",
-              width = 0.2,
-              shape = 21)+
-  ylim(0,2)+ 
-  labs(x = "Diet \n(Protein; Carbohydrate)",
-       y = "Mean (+/- S.E.) number of flies")+
-  theme_minimal()
-
-
-#-------- Females (Day 2)
-
-
-females_mf_e3_d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE3D2(M+F).xlsx", na = "NA"))
-
-long_females_mf_e3_d2 <- females_mf_e3_d2 %>% 
-  pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
-
-females_mf_e3_d2_summary <- long_females_mf_e3_d2 %>% 
-  group_by(diet) %>% 
-  summarise(mean = mean(fly_numbers),
-            sd = sd(fly_numbers),
-            n = n(),
-            se = sd/sqrt(n))
-
-
-females_mf_e3_d2_plot <- females_mf_e3_d2_summary  %>% 
-  ggplot(aes(x = diet, y = mean))+
-  geom_bar(stat = "identity",
-           fill = "red",
-           colour = "blue",
-           alpha = 0.6)+
-  geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
-                colour = "blue",
-                width = 0.2)+
-  geom_jitter(data = long_females_mf_e3_d2,
-              aes(x = diet,
-                  y = fly_numbers),
-              fill = "skyblue",
-              colour = "black",
-              width = 0.2,
-              shape = 21)+
-  ylim(0,2)+ 
-  labs(x = "Diet \n(Protein; Carbohydrate)",
-       y = "Mean (+/- S.E.) number of flies")+
-  theme_minimal()
-
 
 
 
@@ -333,12 +159,218 @@ exp3bothall <- rbind(exp3both1, exp3both2)
 exp3all <- rbind(exp3femalesall, exp3bothall)
 
 exp3all <- exp3all %>% mutate(fly_prop = if_else(variable =="females", 
-                                     fly_numbers/10,
-                                     fly_numbers/5))
+                                                 fly_numbers/10,
+                                                 fly_numbers/5))
 
 
 # linear model with interaction effect
 exp3allls <- glm(fly_numbers ~ diet * variable + day, data = exp3all, family = poisson())
 # use quasi likelihood as null/df >1 quasipoisson()
 performance::check_model(exp3allls)
+
+
+
+
+#- PROBALY IGNORE ALL THIS HASHTAGGED CODE #---------------------------------------
+#--------------------- Mated females (exp 3, day 1)
+#mated_femalese3d1 <- (read_excel(path = "data/MatedFemalesE3D1.xlsx", na = "NA"))
+#long_mated_femalese3d1 <- mated_femalese3d1 %>% 
+#pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
+#long_mated_femalese3d1_summary <- long_mated_femalese3d1 %>% 
+#group_by(diet) %>% 
+#summarise(mean = mean(fly_numbers),
+#sd = sd(fly_numbers),
+#n = n(),
+# se = sd/sqrt(n))
+#--------------------- Visualising the data for mated females (exp 3, day 1)
+#mated_femalese3d1_plot <- long_mated_femalese3d1_summary%>% 
+#ggplot(aes(x = diet, y = mean))+
+#geom_bar(stat = "identity",
+#          fill = "skyblue",
+#          colour = "orange",
+#          alpha = 0.6)+
+# geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+#               colour = "orange",
+#               width = 0.2)+
+# geom_jitter(data = long_mated_femalese3d1,
+#             aes(x = diet,
+#                 y = fly_numbers),
+#             fill = "skyblue",
+#             colour = "black",
+#             width = 0.2,
+#             shape = 21)+
+# ylim(0,6)+ 
+# labs(x = "Diet \n(Protein; Carbohydrate)",
+#      y = "Mean (+/- S.E.) number of flies")+
+# theme_minimal()
+#--------- Data analysis for mated females (exp 3, day 1)
+#--------------------- Mated females (exp 3, day 2)
+#mated_femalese3d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE3D2.xlsx", na = "NA"))
+#
+#long_mated_femalese3d2 <- mated_femalese3d2 %>% 
+#pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
+#long_mated_femalese3d2_summary <- long_mated_femalese3d2 %>% 
+# group_by(diet) %>% 
+# summarise(mean = mean(fly_numbers),
+#           sd = sd(fly_numbers),
+#           n = n(),
+#  se = sd/sqrt(n))
+
+#--------------  Visualising the data for mated females (exp 3, day 2)
+
+#mated_femalese3d2_plot <- long_mated_femalese3d2_summary%>% 
+# ggplot(aes(x = diet, y = mean))+
+# geom_bar(stat = "identity",
+#          fill = "skyblue",
+#          colour = "orange",
+#          alpha = 0.6)+
+# geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+#               colour = "orange",
+#               width = 0.2)+
+# geom_jitter(data = long_mated_femalese3d2,
+#             aes(x = diet,
+#   y = fly_numbers),
+#             fill = "skyblue",
+#             colour = "black",
+#             width = 0.2,
+#             shape = 21)+
+# ylim(0,6)+ 
+# labs(x = "Diet \n(Protein; Carbohydrate)",
+#      y = "Mean (+/- S.E.) number of flies")+
+# theme_minimal()
+#-------- Data analysis for mated females (exp 3, day 2)
+#---------------- Males + Females ----- -----------
+#----------------------------- Males (m+f) (exp 3, day 1)
+#males_mf_e3_d1 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedMalesE3D1(M+F).xlsx", na = "NA"))
+#long_males_mf_e3_d1 <- males_mf_e3_d1 %>% 
+#pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
+#males_mf_e3_d1_summary <- long_males_mf_e3_d1 %>% 
+#group_by(diet) %>% 
+# summarise(mean = mean(fly_numbers),
+#           sd = sd(fly_numbers),
+#           n = n(),
+#           se = sd/sqrt(n))
+#------------------- Visualising the data for males (exp 3, day 1)
+#males_mf_e3_d1_plot <- males_mf_e3_d1_summary  %>% 
+# ggplot(aes(x = diet, y = mean))+
+# geom_bar(stat = "identity",
+#  fill = "red",
+#          colour = "blue",
+#          alpha = 0.6)+
+# geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+#               colour = "blue",
+#               width = 0.2)+
+# geom_jitter(data = long_males_mf_e3_d1,
+#             aes(x = diet,
+#                 y = fly_numbers),
+#             fill = "skyblue",
+#             colour = "black",
+#             width = 0.2,
+#             shape = 21)+
+# ylim(0,2)+ 
+# labs(x = "Diet \n(Protein; Carbohydrate)",
+#      y = "Mean (+/- S.E.) number of flies")+
+# theme_minimal()
+#------------------------- Females (m+f) (exp 3, day 1) 
+#females_mf_e3_d1 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE3D1(M+F).xlsx", na = "NA"))
+#long_females_mf_e3_d1 <- females_mf_e3_d1 %>% 
+# pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
+
+#females_mf_e3_d1_summary <- long_females_mf_e3_d1 %>% 
+#  group_by(diet) %>% 
+# summarise(mean = mean(fly_numbers),
+#           sd = sd(fly_numbers),
+#           n = n(),
+#           se = sd/sqrt(n))
+#---------------------- Visualising the data for females (exp 3, day 1)
+#females_mf_e3_d1_plot <- females_mf_e3_d1_summary  %>% 
+# ggplot(aes(x = diet, y = mean))+
+# geom_bar(stat = "identity",
+#          fill = "red",
+#          colour = "blue",
+#          alpha = 0.6)+
+# geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+#               colour = "blue",
+#               width = 0.2)+
+# geom_jitter(data = long_females_mf_e3_d1,
+#             aes(x = diet,
+#   y = fly_numbers),
+#             fill = "skyblue",
+#             colour = "black",
+#             width = 0.2,
+#             shape = 21)+
+# ylim(0,2)+ 
+# labs(x = "Diet \n(Protein; Carbohydrate)",
+#      y = "Mean (+/- S.E.) number of flies")+
+# theme_minimal()
+#-------- Plot showing males and females together (Day 1)
+#males_mf_e3_d1_plot + females_mf_e3_d1_plot
+#-------- Males (m+f) (exp 3,Day 2)
+#-males_mf_e3_d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedMalesE3D2(M+F).xlsx", na = "NA"))
+
+#long_males_mf_e3_d2 <- males_mf_e3_d2 %>% 
+#pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
+#males_mf_e3_d2_summary <- long_males_mf_e3_d2 %>% 
+#group_by(diet) %>% 
+# summarise(mean = mean(fly_numbers),
+#           sd = sd(fly_numbers),
+#           n = n(),
+#           se = sd/sqrt(n))
+#--------  Visualising the data for males (m+f) (exp 3, day 2)
+#males_mf_e3_d2_plot <- males_mf_e3_d2_summary  %>% 
+# ggplot(aes(x = diet, y = mean))+
+# geom_bar(stat = "identity",
+#          fill = "red",
+#          colour = "blue",
+#          alpha = 0.6)+
+# geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+#               colour = "blue",
+#           width = 0.2)+
+# geom_jitter(data = long_males_mf_e3_d2,
+#             aes(x = diet,
+#                 y = fly_numbers),
+#             fill = "skyblue",
+#             colour = "black",
+#             width = 0.2,
+#             shape = 21)+
+#  ylim(0,2)+ 
+# labs(x = "Diet \n(Protein; Carbohydrate)",
+#      y = "Mean (+/- S.E.) number of flies")+
+# theme_minimal()
+#-------- Females (Day 2)
+#females_mf_e3_d2 <- (read_excel(path = "~/Documents/drosophilaresearchproject/data/MatedFemalesE3D2(M+F).xlsx", na = "NA"))
+#long_females_mf_e3_d2 <- females_mf_e3_d2 %>% 
+# pivot_longer(cols = ("8;1":"1;8"), names_to = "diet", values_to = "fly_numbers")
+#females_mf_e3_d2_summary <- long_females_mf_e3_d2 %>% 
+# group_by(diet) %>% 
+# summarise(mean = mean(fly_numbers),
+#           sd = sd(fly_numbers),
+#           n = n(),
+#            se = sd/sqrt(n))
+#females_mf_e3_d2_plot <- females_mf_e3_d2_summary  %>% 
+#  ggplot(aes(x = diet, y = mean))+
+#  geom_bar(stat = "identity",
+#          fill = "red",
+#           colour = "blue",
+#           alpha = 0.6)+
+#geom_errorbar(aes(ymin = mean-se, ymax = mean+se), 
+#                colour = "blue",
+#               width = 0.2)+
+#geom_jitter(data = long_females_mf_e3_d2,
+#             aes(x = diet,
+#            y = fly_numbers),
+#             fill = "skyblue",
+#            colour = "black",
+#            width = 0.2,
+#             shape = 21)+
+# ylim(0,2)+ 
+# labs(x = "Diet \n(Protein; Carbohydrate)",
+#      y = "Mean (+/- S.E.) number of flies")+
+# theme_minimal()
+
+
+
+
+
+
 
